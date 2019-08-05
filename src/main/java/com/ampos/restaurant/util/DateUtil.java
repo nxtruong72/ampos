bf6405d5e4061b7c029c17a7414a6d00e22ec1e8
Truong Nguyen <nxuantruong@partners.accedian.com>
2019-08-05 10:35:07 +0700

    XXX

--- src/main/java/com/ampos/restaurant/util/DateUtil.java
+++ src/main/java/com/ampos/restaurant/util/DateUtil.java
@@ -9,11 +9,25 @@ public class DateUtil {
 	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 
 	/**
-	 * convert String to date
+	 * Convert String to Date
+	 * @param date
+	 * @param timeZone
+	 * @return
+	 * @throws ParseException
 	 */
 	public static Date convertStringToDate(String date,String timeZone) throws ParseException {
 		DATE_FORMAT.setTimeZone(TimeZone.getTimeZone(timeZone));
 		return DATE_FORMAT.parse(date);
 	}
 
+	/**
+	 * Convert Date to String
+	 * @param date
+	 * @param timezone
+	 * @return
+	 */
+	public static String convertDateToString(Date date, String timezone) {
+		DATE_FORMAT.setTimeZone(TimeZone.getTimeZone(timezone));
+		return DATE_FORMAT.format(date);
+	}
 }
